a7949c6a1fa52262bb9438908fa5bf2a04c924a2
Naufal Asidiq <naufalasidiq150@gmail.com>
2026-05-17 09:02:44 +0700

    fix ecs port(2)

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ app.post("/submit", upload.single("file"), async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server berjalan di port ${PORT}`);
 });
